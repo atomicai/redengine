3304d4afe8946dcb5f74ca2859380c7a2d54012d
Alexey <kiselev.alexey.sergeevich1@yandex.ru>
2024-09-25 01:35:08 +0300

    add script to select bad data

--- redengine/dataLoads/false_data.py
+++ redengine/dataLoads/false_data.py
@@ -61,7 +61,7 @@ def contains_russian(text):
         return any('\u0400' <= char <= '\u04FF' for char in text)
     return False
 
-def save_to_json(data, filename='data_for_fix33.json'):
+def save_to_json(data, filename='data_for_fix.json'):
     with open(filename, 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
 
